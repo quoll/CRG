ba649bb514a28ae441752fe1e4dd450e981a3d8b
Paula Gearon <pgearon@cisco.com>
2016-08-19 16:46:40 -0400

    Restructured project

--- project.clj
+++ project.clj
@@ -1,28 +1,10 @@
-(defproject org.mulgara/crg "0.0.1-SNAPSHOT"
+(defproject org.mulgara/crg "0.0.2-SNAPSHOT"
 
   ;; Project Description
   :description "Clojure RDF Graphs"
 
-  ;; Project dependencies, resolved via Maven
-  ;; Format is: [<groupId>/<artifactId> "version"]
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [org.mulgara/mrg "0.7"]]
-
-  :dev-dependencies [[lein-dist "0.0.18"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
 
   :aot [mulgara.crg.core]
 
-  ;; Use a Maven-like directory structure
-  :source-path "src/main/clj"
-  :test-path "src/test/clj"
-
-  :target-dir "target"
-  :compile-path "target/classes"
-  
-  :jar-distdir "lib/"
-  :pkg [;;include       excludes?    dest
-        ["lib/*.jar"                 "lib/ext/"]
-        ["src/dist/*"                ""]]
-  
   )
